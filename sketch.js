24f81e4cf37351084f737264d4b48f01e24e256a
Dabarkle <davidb@admins-mbp.lan>
2019-03-02 22:17:21 +1100

    new

--- sketch.js
+++ sketch.js
@@ -1,12 +1,21 @@
-var myCanvas = {
+var myC = {
 x: 600,
 y: 400
+}
+var ctPoint = {
+    x: 300,
+    y: 200
 }
 
 
 function setup() {
-    var myCanvas = createCanvas(myCanvas.x, myCanvas.y);
+    var myCanvas = createCanvas(myC.x, myC.y);
     background(100);
     myCanvas.parent('myContainer');
-    frameRate(300);
+    
 }
+function draw(){
+
+    }
+
+
