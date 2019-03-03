425a9c8bb5e9ee05444fa2ffd8f3051bf767a0b5
Dabarkle <davidb@admins-mbp.lan>
2019-03-03 12:33:58 +1100

    new

--- sketch.js
+++ sketch.js
@@ -7,15 +7,40 @@ var ctPoint = {
     y: 200
 }
 
-
+var x = 0;
+var speed = 3;
+var y = 100;
 function setup() {
     var myCanvas = createCanvas(myC.x, myC.y);
-    background(100);
     myCanvas.parent('myContainer');
+    frameRate(300);
     
 }
 function draw(){
+    background (0);
+    stroke(500);
+    cColor();
+    strokeWeight(4);
+    noFill();
+    y = 100;
+    y = y + random(100,120);
+    ellipse(x ,y,20,20);
+    eBounce();
+    x = x + speed;
 
     }
 
 
+function eBounce(){
+    if( x > width){
+        speed = -5;
+
+    } else if (x < 0){
+        speed = 5
+    }
+}
+
+function cColor(){
+    if(x > width / 2)
+    stroke(100);
+}
